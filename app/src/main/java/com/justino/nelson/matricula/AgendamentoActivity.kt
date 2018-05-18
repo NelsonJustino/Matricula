e8ed35481afe54fc926b4db53d69249d12afa5e2
logonrm <logonrm@fiap.com.br>
2018-05-18 09:38:26 -0300

    Initial commit

---
+++ app/src/main/java/com/justino/nelson/matricula/AgendamentoActivity.kt
@@ -0,0 +1,12 @@
+package com.justino.nelson.matricula
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendamentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agendamento)
+    }
+}
